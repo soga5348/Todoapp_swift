2e1481b5ae993a761609fd1e05e3f981809975d2
Your Name <your_email@example.com>
2023-10-17 20:03:49 +0900

    first-commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by soga syunto on 2023/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
